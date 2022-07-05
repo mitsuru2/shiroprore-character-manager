09c391f47005a2fda4965a22aed7d0d7e45a950b
mitsuru2 <mitsuru.takahashi@live.jp>
2022-07-05 22:20:35 +0900

    I finished the project migration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-oshiro-2',
-    appId: '1:698065163413:web:41586f54e17a79fd1db79c',
-    storageBucket: 'angular-oshiro-2.appspot.com',
-    apiKey: 'AIzaSyBwj0VmBpaHxbWv7kawff5FZ9FO1fP7c8c',
-    authDomain: 'angular-oshiro-2.firebaseapp.com',
-    messagingSenderId: '698065163413',
-    measurementId: 'G-953GC66ELT',
+    projectId: 'shiroprore-character',
+    appId: '1:781031014078:web:d20c36b3bd65ce6ca7a8ef',
+    storageBucket: 'shiroprore-character.appspot.com',
+    apiKey: 'AIzaSyAlwgn8ADmsK82EskkQ7WOLUQGsw1Kxegk',
+    authDomain: 'shiroprore-character.firebaseapp.com',
+    messagingSenderId: '781031014078',
+    measurementId: 'G-60DCGDQGFJ',
   },
   production: true,
 };
