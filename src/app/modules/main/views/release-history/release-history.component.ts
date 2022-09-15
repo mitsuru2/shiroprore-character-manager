7a6abefd6069d8cfabb694f19ce76b16a404caed
mitsuru2 <mitsuru.takahashi@live.jp>
2022-09-15 19:48:39 +0900

    Character list page and character page shows the iplemented date information.

--- src/app/modules/main/views/release-history/release-history.component.ts
+++ src/app/modules/main/views/release-history/release-history.component.ts
@@ -44,7 +44,7 @@ export class ReleaseHistoryComponent implements OnInit {
     // Make date text.
     for (let i = 0; i < this.filteredVersions.length; ++i) {
       this.logger.debug(location, { seconds: (this.filteredVersions[i].createdAt as Timestamp).seconds });
-      const date = new Date((this.filteredVersions[i].createdAt as Timestamp).seconds * 1000);
+      const date = this.firestore.convTimestampToDate(this.filteredVersions[i].createdAt);
       this.createdDates.push(date.toLocaleDateString());
     }
   }
