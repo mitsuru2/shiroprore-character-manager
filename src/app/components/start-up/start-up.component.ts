1c434ec827d7ba42d09186fcd1f607c2aef64a35
mitsuru2 <mitsuru.takahashi@live.jp>
2022-09-19 14:38:40 +0900

    I changed data loading timing of thumbnail images. It now load first 50 thumbnail images at the start page.

--- src/app/components/start-up/start-up.component.ts
+++ src/app/components/start-up/start-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NGXLogger } from 'ngx-logger';
 import { AppInfo } from 'src/app/app-info.enum';
+import { CloudStorageService } from 'src/app/services/cloud-storage/cloud-storage.service';
 import { ErrorHandlerService } from 'src/app/services/error-handler/error-handler.service';
 import { FirestoreDataService } from 'src/app/services/firestore-data/firestore-data.service';
 
@@ -15,6 +16,8 @@ export class StartUpComponent implements OnInit {
 
   readonly interval = 200; // ms.
 
+  readonly loadThumbNum = 50;
+
   appInfo = AppInfo;
 
   loaded = false;
@@ -23,7 +26,8 @@ export class StartUpComponent implements OnInit {
     private logger: NGXLogger,
     private errorHandler: ErrorHandlerService,
     private router: Router,
-    private firestore: FirestoreDataService
+    private firestore: FirestoreDataService,
+    private storage: CloudStorageService
   ) {
     this.logger.trace(`new ${this.className}()`);
   }
@@ -31,6 +35,10 @@ export class StartUpComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     const location = `${this.className}.ngOnInit()`;
     this.logger.trace(location);
+
+    // Load thumbnail images.
+    await this.loadThumbnailImages();
+    this.logger.info(location, 'Thumbnail images are loaded.');
 
     // Wait until that the firestore data loading will be finished.
     this.waitFirestoreDataLoading();
@@ -56,5 +64,15 @@ export class StartUpComponent implements OnInit {
         clearInterval(timer);
       }
     }, this.interval);
+  }
+
+  private async loadThumbnailImages(): Promise<void> {
+    let promises: Promise<Blob>[] = [];
+
+    for (let i = 0; i < this.loadThumbNum; ++i) {
+      const path = this.storage.makeCharacterThumbnailPath(`10-00-${i.toString(16).padStart(4, '0')}`);
+      promises.push(this.storage.get(path));
+    }
+    await Promise.all(promises);
   }
 }
