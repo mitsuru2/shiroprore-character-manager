6d152e29a530a78712da91cd17a9a86d6167044e
mitsuru2 <mitsuru.takahashi@live.jp>
2022-10-01 09:05:35 +0900

    I incremented the minor version.

--- src/app/app-info.enum.ts
+++ src/app/app-info.enum.ts
@@ -1,7 +1,7 @@
 export enum AppInfo {
   title = '御城プロジェクト:RE',
   subTitle = 'キャラクター管理ツール',
-  version = '1.5.0',
+  version = '1.5.1',
   baseUrlProd = 'https://shiroprore-character.web.app',
   baseUrlDev = 'http://localhost:4200',
 }
