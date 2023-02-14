4615d267fa5a677e65d70a941c22afc398362d3b
mitsuru2 <mitsuru.takahashi@live.jp>
2023-02-14 19:30:58 +0900

    I removed unused file.

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -26,13 +26,27 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
 import { FieldsetModule } from 'primeng/fieldset';
 
+/** Firebase modules. */
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
+import { environment } from 'src/environments/environment';
+
 /** Application components. */
-import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { MainRoutingModule } from './main-routing.module';
 import { NewCharacterComponent } from './components/new-character/new-character.component';
 import { ListCharacterComponent } from './components/list-character/list-character.component';
 import { LegalComponent } from './components/legal/legal.component';
 import { LoginComponent } from './components/login/login.component';
+import { CharacterComponent } from './components/character/character.component';
+import { ListCharacterOwnershipComponent } from './components/list-character-ownership/list-character-ownership.component';
+import { SupportComponent } from './components/support/support.component';
+import { TeamComponent } from './components/team/team.component';
+import { ListCharacterKaichikuComponent } from './components/list-character-kaichiku/list-character-kaichiku.component';
+import { NavigatorService } from './services/navigator/navigator.service';
+import { UserAuthService } from './services/user-auth/user-auth.service';
+import { SpinnerService } from './services/spinner/spinner.service';
 import { TopMenuComponent } from './views/top-menu/top-menu.component';
 import { TopMenuMComponent } from './views/top-menu-m/top-menu-m.component';
 import { SideMenuComponent } from './views/side-menu/side-menu.component';
@@ -41,32 +55,19 @@ import { NewWeaponFormComponent } from './views/new-weapon-form/new-weapon-form.
 import { NewFacilityFormComponent } from './views/new-facility-form/new-facility-form.component';
 import { NewCharacterConfirmationComponent } from './views/new-character-confirmation/new-character-confirmation.component';
 import { MakeThumbnailFormComponent } from './views/make-thumbnail-form/make-thumbnail-form.component';
-
-/** Firebase modules. */
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
-import { environment } from 'src/environments/environment';
-import { NavigatorService } from './services/navigator/navigator.service';
-import { UserAuthService } from './services/user-auth/user-auth.service';
-import { CharacterComponent } from './components/character/character.component';
 import { CharacterFilterSettingsFormComponent } from './views/character-filter-settings-form/character-filter-settings-form.component';
-import { SpinnerService } from './services/spinner/spinner.service';
 import { CharacterSortSettingsFormComponent } from './views/character-sort-settings-form/character-sort-settings-form.component';
 import { CopyrightNoticeComponent } from './views/copyright-notice/copyright-notice.component';
 import { PrivacyPolicyComponent } from './views/privacy-policy/privacy-policy.component';
 import { TermsOfServiceComponent } from './views/terms-of-service/terms-of-service.component';
 import { RemoveUserDataComponent } from './views/remove-user-data/remove-user-data.component';
-import { ListCharacterOwnershipComponent } from './components/list-character-ownership/list-character-ownership.component';
 import { CharacterFilterService } from './services/character-filter/character-filter.service';
-import { SupportComponent } from './components/support/support.component';
 import { InquiryFormComponent } from './views/inquiry-form/inquiry-form.component';
 import { ReleaseHistoryComponent } from './views/release-history/release-history.component';
 import { ReadMeComponent } from './views/read-me/read-me.component';
 import { AnnounceComponent } from './views/announce/announce.component';
-import { TeamComponent } from './components/team/team.component';
 import { TeamViewComponent } from './views/team-view/team-view.component';
-import { ListCharacterKaichikuComponent } from './components/list-character-kaichiku/list-character-kaichiku.component';
+import { DynamicHelpComponent } from './views/dynamic-help/dynamic-help.component';
 
 const firebaseUiAuthConfig: firebaseui.auth.Config = {
   autoUpgradeAnonymousUsers: false, // Disable annymouse users.
@@ -114,6 +115,7 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
     TeamComponent,
     TeamViewComponent,
     ListCharacterKaichikuComponent,
+    DynamicHelpComponent,
   ],
   imports: [
     CommonModule,
