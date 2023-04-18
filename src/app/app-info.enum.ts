1826505185356ab4fda46f369fc5017a50be66f8
mitsuru2 <mitsuru.takahashi@live.jp>
2023-04-18 20:52:09 +0900

    I increased the middle version.

--- src/app/app-info.enum.ts
+++ src/app/app-info.enum.ts
@@ -1,7 +1,7 @@
 export enum AppInfo {
   title = '御城プロジェクト:RE',
   subTitle = 'キャラクター管理ツール',
-  version = '2.0.2',
+  version = '2.1.0',
   baseUrlProd = 'https://shiroprore-character.web.app',
   baseUrlDev = 'http://localhost:4200',
 }
